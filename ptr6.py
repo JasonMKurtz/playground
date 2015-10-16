e990f491319ce3ff3718fc284be55e97849e149c
Jason Kurtz <jason.kurtz@jcrew.com>
2015-10-16 16:37:39 -0400

    Comments.

--- ptr6.py
+++ ptr6.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python
+"""
+Usage: ./ptr6.py <ipv6 address>
+Example: 
+
+$ ./ptr6.py 2001:470:b995:a019::1
+1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.9.1.0.a.5.9.9.b.0.7.4.0.1.0.0.2.in-addr.arpa.
+
+"""
 import sys
 def ptr6(str): 
     return "%s.in-addr.arpa." % (".".join([ ".".join(list(oct.zfill(4)[::-1])) for oct in str.replace("::", "".join([ ":" for i in range(9 - len([ oct for oct in str.split(":") if oct != '' ])) ])).split(":") ][::-1]))
