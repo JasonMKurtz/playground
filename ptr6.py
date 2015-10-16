357c7ac0f4d2f5cc79681d314ef6eb6eaefe7a9b
Jason Kurtz <jason.kurtz@jcrew.com>
2015-10-16 17:05:08 -0400

    got the suffix wrong

--- ptr6.py
+++ ptr6.py
@@ -9,6 +9,6 @@ $ ./ptr6.py 2001:470:b995:a019::1
 """
 import sys
 def ptr6(str): 
-    return "%s.in-addr.arpa." % (".".join([ ".".join(list(oct.zfill(4)[::-1])) for oct in str.replace("::", "".join([ ":" for i in range(9 - len([ oct for oct in str.split(":") if oct != '' ])) ])).split(":") ][::-1]))
+    return "%s.ip6.arpa." % (".".join([ ".".join(list(oct.zfill(4)[::-1])) for oct in str.replace("::", "".join([ ":" for i in range(9 - len([ oct for oct in str.split(":") if oct != '' ])) ])).split(":") ][::-1]))
 
 print ptr6("".join(sys.argv[1::]))
