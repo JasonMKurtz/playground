98e8e818932e96914ceef1d1801f411d3b860229
Jason Kurtz <jason@jkurtz.net>
2015-11-05 15:20:38 -0500

    update

--- comma.py
+++ comma.py
@@ -1,4 +1,6 @@
 #!/usr/bin/python
 import sys
+""" Input needs to be changed to sys.argv[1::] if we are reading arguments. """ 
+input = sys.stdin.read() 
 def msplice(num): return ''.join([ d.replace(d, "%s%s" % (d, "," if pos != 0 and (pos % 3) == 0 else "")) for pos, d in enumerate(num[::-1]) ][::-1])
-print msplice(''.join(sys.argv[1::]))
+print msplice(input.strip())
