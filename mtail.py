02d112a58f0649fd98843984e31f1116264d0b17
Jason Kurtz <jason@jkurtz.net>
2016-01-02 12:43:37 -0500

    make sure we have enough full lines

--- mtail.py
+++ mtail.py
@@ -3,18 +3,23 @@ class File:
     def __init__(self, filename, blocksize=1024): 
         self.file = filename
         self.block = blocksize
+        self.reads = 0
+
+    def numreads(self): 
+        return self.reads
 
     def tail(self, lines2find): 
         with open(self.file, 'r') as f: 
             f.seek(0, 2)
             total_bytes = f.tell()
             scanned, lines = 0, 0
-            while (total_bytes > scanned and lines2find + 1 > lines): 
+            while (total_bytes > scanned and lines2find + 2 >= lines): 
                 byte_block = min(self.block, total_bytes - scanned)
                 f.seek(-byte_block, 2)
                 scanned += byte_block
                 lines += f.read().count("\n")
+                self.reads += 1
             f.seek(-scanned, 2)
             return '\n'.join([ line.strip() for line in f.readlines()[-lines2find:] ])
 
-print File("abc").tail(10)        
+print File("abc", 10).tail(10)        
