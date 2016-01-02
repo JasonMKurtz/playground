99332021b624182dd24929b492606ecd05a7166d
Jason Kurtz <jason@jkurtz.net>
2016-01-02 10:15:42 -0500

    classify it

--- mtail.py
+++ mtail.py
@@ -1,16 +1,20 @@
 #!/usr/bin/python
-def tail(lines2find): 
-    file = "abc"
-    block = 1024
-    with open(file, 'r') as f: 
-        f.seek(0, 2)
-        total_bytes = f.tell()
-        scanned, lines = 0, 0
-        while (total_bytes > scanned and lines2find + 1 > lines): 
-            byte_block = min(block, total_bytes - scanned)
-            f.seek(-byte_block, 2)
-            scanned += byte_block
-            lines += f.read().count("\n")
-        f.seek(-scanned, 2)
-        return '\n'.join([ line.strip() for line in f.readlines()[-lines2find:] ])
-print tail(10)        
+class File: 
+    def __init__(self, filename, blocksize=1024): 
+        self.file = filename
+        self.block = blocksize
+
+    def tail(self, lines2find): 
+        with open(self.file, 'r') as f: 
+            f.seek(0, 2)
+            total_bytes = f.tell()
+            scanned, lines = 0, 0
+            while (total_bytes > scanned and lines2find + 1 > lines): 
+                byte_block = min(self.block, total_bytes - scanned)
+                f.seek(-byte_block, 2)
+                scanned += byte_block
+                lines += f.read().count("\n")
+            f.seek(-scanned, 2)
+            return '\n'.join([ line.strip() for line in f.readlines()[-lines2find:] ])
+
+print File("abc").tail(10)        
