421e6983ef1227c52b8556b725b85a4305f52c97
Jason Kurtz <jason.kurtz@jcrew.com>
2016-02-11 14:55:39 -0500

    variable size

--- ascii.py
+++ ascii.py
@@ -1,3 +1,4 @@
 #!/usr/bin/python 
 """ Some nice ascii art. """
-print "\n".join([ "{a}{b}{a}".format(a="*"*(30-i), b="||"*(i)) for i in range(1, 30) ]), "\n", "\n".join([ "{a}{b}{a}".format(a="*"*i, b="||"*(30-i)) for i in range(2, 30) ])
+size = 15
+print "\n".join([ "{a}{b}{a}".format(a="*"*(size-i), b="||"*(i)) for i in range(1, size) ]), "\n", "\n".join([ "{a}{b}{a}".format(a="*"*i, b="||"*(size-i)) for i in range(2, size) ])
