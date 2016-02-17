05082abe4b6f29ab45b8c6c1106fe57b1c2fdbee
Jason Kurtz <jason.kurtz@jcrew.com>
2016-02-17 11:37:08 -0500

    cmdline args for art size.

--- ascii.py
+++ ascii.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python 
 """ Some nice ascii art. """
-size = 15
+import sys
+size = int(sys.argv[1]) if len(sys.argv) > 1 else 15
 print "\n".join([ "{a}{b}{a}".format(a="*"*(size-i), b="||"*(i)) for i in range(1, size) ]), "\n", "\n".join([ "{a}{b}{a}".format(a="*"*i, b="||"*(size-i)) for i in range(2, size) ])
