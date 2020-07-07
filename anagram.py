3a051c5e32b862281e035806833fdaa77822ce04
Jason Kurtz <jason@jkurtz.net>
2020-07-07 10:45:14 -0400

    We need to check all the pairs

--- anagram.py
+++ anagram.py
@@ -5,7 +5,7 @@ import string
 def isAnagram(word):
     print("Checking: %s" % (word))
     word = word.replace(" ", "")
-    for i in range(len(word) / 2):
+    for i in range(len(word)):
         li = -(i + 1)
         print("Checking the pair (%s, %s)" % (word[i], word[li]))
         if string.lower(word[i]) != string.lower(word[li]):
