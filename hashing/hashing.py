35b6544d8e699649c7c07727aadb11c52d9f39a8
Jason Kurtz <jason@jkurtz.net>
2020-10-07 11:30:08 -0400

    working hashtable

--- hashing/hashing.py
+++ hashing/hashing.py
@@ -2,20 +2,28 @@
 
 from typing import Dict, List
 
+class HashTableLimitException(Exception):
+    pass
+
+
 class Table:
     items: Dict[int, List[str]] = {}
     size: int
+    elements: int
 
     def __init__(self, size: int):
         self.size = size
+        self.elements = 0
         pass
 
     def hash(self, value: str) -> str:
-        return str(len(value) % self.size) 
+        return hash(value) % self.size
     
     def add(self, value: str) -> None:
-        if len(self.items) > self.size:
-            return
+        if self.elements >= self.size:
+            raise HashTableLimitException(f"{self.size} items already exist.")
+
+        self.elements += 1
 
         index: int = self.hash(value)
         if index in self.items:
@@ -24,16 +32,22 @@ class Table:
             update: List[str] = [value]
         
         self.items.update({index: update})
-        print(self)
 
     def __repr__(self) -> str:
         return '\n'.join([
             f"{self.items[k]} -> {k}"
             for k in self.items
         ])
+    
+    def __len__(self) -> int:
+        return self.elements
 
     def lookup(self, value: str) -> (int, str):
         hash: int = self.hash(value)
+        items: List[str] = self.items[hash] or []
+        if value not in items:
+            return -1, ""
+
         return hash, self.items[hash] or ''
         
 
