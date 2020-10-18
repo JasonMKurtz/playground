b114da81fda75017fc1510076d89f801ea18d490
Jason Kurtz <jason@jkurtz.net>
2020-10-17 21:45:01 -0400

    determine if a tree is a binary search tree

--- practice/trees/binary_tree.py
+++ practice/trees/binary_tree.py
@@ -1,29 +1,51 @@
 #!/usr/local/bin/python3 
 
-from typing import Dict, List, Set
+from typing import Dict, List, Set, Optional
 from queue import Queue
 
 class Graph:
     pairs: Dict[str, List[str]] = {}
     def __init__(self):
         self.pairs = {
-            0: set([1, 2]),
-            1: set([0, 3]),
-            2: set([0, 3]),
-            3: set([1, 4]),
+            10: [5, 12],
+            5: [2, 11],
+            12: [11, 17],
         }
     
-    def is_binary_tree(self, item, seen={}) -> bool:
+    def is_bst(self, start) -> bool:
+        # For every subtree, [0] < root < [1]
+        node = self.pairs.get(start)
+        if node is None:
+            return True
+
+        if len(node) > 2:
+            return False
+    
+        left = node[0]
+        right = node[1]
+
+        if left is not None and left > start:
+            return False
+
+        if right is not None and right < start:
+            return False
+
+        if not self.is_bst(node[0]) or not self.is_bst(node[1]):
+            return False
+
+        return True
+    
+    def is_binary_tree(self, start) -> bool:
         # Does our node exist?
         # Does our node have more than two neighbors?
         # Do any of its neighbors have more than two neighbors?
 
-        node = self.pairs.get(item)
+        node = self.pairs.get(start)
         if node is None or len(node) > 2:
             return False
 
         visited = Queue()
-        visited.put(item)
+        visited.put(start)
         seen = {}
         while not visited.empty():
             i = visited.get()
